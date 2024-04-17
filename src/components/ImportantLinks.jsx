c38c0b2c7dd5ecc8bde1d4af18ccf097a8c6024a
Krestine H <krha0001@stud.kea.dk>
2024-04-17 23:47:36 +0200

    small update

--- src/components/ImportantLinks.jsx
+++ src/components/ImportantLinks.jsx
@@ -1,5 +1,35 @@
 import styles from "../styles/ImportantLinks.module.css";
+import Image from "next/image";
 
 export default function ImportantLinks() {
-  return <h2>Links go here</h2>;
+  return (
+    <ul className={styles.layout}>
+      <li className={styles.link}>
+        <a
+          href="https://www.linkedin.com/in/krestine-haugaard-a80447264/"
+          target="_blank"
+        >
+          <Image
+            className={styles.icon}
+            src="/svgs/github.svg"
+            alt="Github icon"
+            width={917}
+            height={1056}
+          />
+        </a>
+      </li>
+      <li className={styles.link}>
+        <a href="https://github.com/krestineHaugaard" target="_blank">
+          git
+        </a>
+      </li>
+      <li className={styles.link}>
+        <a href="mailto:krestinehaugaard@gmail.com">mail</a>
+      </li>
+      <li className={styles.link}>
+        <a href="tel:+45.30.25.09.85">telefon</a>
+      </li>
+      <li></li>
+    </ul>
+  );
 }
